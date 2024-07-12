9805e77334b1f41c0fb061ab3156802f515d1619
Eswar Reddy Boyi <eswarreddyboyi7@gmail.com>
2024-07-12 21:39:56 +0530

    Add files via upload
    changed  done in middleware auth code

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
+// Middleware function to authenticate JWT token
 module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
@@ -13,9 +14,11 @@ module.exports = function(req, res, next) {
   // Verify token
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
-    req.user = decoded.user;
-    next();
+    req.user = decoded.user; // Set user object in request
+    next(); // Move to next middleware
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
+
+
